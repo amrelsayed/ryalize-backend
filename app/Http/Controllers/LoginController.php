f2d3fcf8176a2603802aabf7eac02491ccc4ba52
Amr Elsayed <amr-elsayed@outlook.com>
2024-10-27 17:09:54 +0300

    add login api

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request): mixed
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            throw ValidationException::withMessages(messages: [
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken($user->name)->plainTextToken;
+    }
+}
